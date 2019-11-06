3e437d00913e4295a8b1f1244d9687c5217d982e
Michael Perks <michael.perks@live.co.uk>
2019-11-06 21:44:25 +0000

    Update redirected route

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/collection';
 
     /**
      * Create a new controller instance.
