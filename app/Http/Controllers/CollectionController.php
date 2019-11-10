79767097a8cd075d217afd6212d0ffba235675c0
Michael Perks <michael.perks@live.co.uk>
2019-11-10 01:31:49 +0000

    wip

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -3,30 +3,43 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Issue;
 use App\Stamp;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class CollectionController extends Controller
 {
     /**
      * Description
-     *  
+     *
      * @param string name
-     * 
+     *
      * @return void
      */
     public function show()
     {
-        $collection = auth()->user()->stamps->groupBy('issue.id');
+        // $collection = auth()->user()->stamps->groupBy('issue.id');
+
+        $collection = Issue::whereHas('stamps')
+                        ->with([
+                            'stamps' => function ($query) {
+                                $query->whereIn('stamp_id', DB::table('collections')
+                                    ->select(DB::raw('stamp_id'))
+                                    ->where('user_id', '=', auth()->user()->id)
+                                    ->get());
+                            }
+                        ])
+                        ->get();
+
         return view('collection.index', compact('collection'));
     }
 
     /**
      * Adds a stamp to the auth user's collection.
-     *  
+     *
      * @param Stamp $stamp
-     * 
+     *
      * @return null
      */
     public function store(Stamp $stamp)
@@ -38,9 +51,9 @@ class CollectionController extends Controller
 
     /**
      * Removes a stamp from the auth user's collection.
-     *  
+     *
      * @param Stamp $stamp
-     * 
+     *
      * @return null
      */
     public function destroy(Stamp $stamp)
--- resources/views/collection/index.blade.php
+++ resources/views/collection/index.blade.php
@@ -4,9 +4,9 @@
     <h1 class="text-4xl border-b mb-6">My Collection</h1>
     @forelse ($collection as $issue)
         <div class="issue mb-5">
-            <h2 class="text-xl border-b p-1 mb-2">{{ $issue->first()->issue->title}}</h2>
+            <h2 class="text-xl border-b p-1 mb-2">{{ $issue->title}}</h2>
             <div class="flex flex-wrap">
-                @forelse ($issue as $stamp)
+                @forelse ($issue->stamps as $stamp)
                     <div class="flex flex-col items-center mr-1 p-1 border w-1/8">
                         <img src="{{ asset($stamp->image_src) }}" alt="{{ $stamp->title }}" class="h-20">
                         <p>{{ $stamp->title }}</p>
