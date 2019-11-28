d9ef595d8b420b60ed29cdcff8e2fc2322513aee
Michael Perks <michael.perks@live.co.uk>
2019-11-28 22:47:31 +0000

    Redirect to collection if Authenticated

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/collection';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,16 +10,6 @@ use App\Collection;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Shows the public front welcome page.
      *
      * @return \Illuminate\Contracts\Support\Renderable
