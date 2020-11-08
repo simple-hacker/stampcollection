95787e683daa30a6c738c1a2913cdde0492412f3
Michael Perks <michael.perks@live.co.uk>
2020-11-08 20:44:30 +0000

    Add titles to top icons

--- resources/js/components/collection/CollectionValues.vue
+++ resources/js/components/collection/CollectionValues.vue
@@ -3,13 +3,13 @@
         <div class="flex justify-between items-center mb-2 p-4 bg-darker text-white">
             <h1 class="text-4xl">My Collection</h1>
             <div class="flex">
-                <a href="/collection/print" class="p-4 hover:text-highlight mr-1" target="_blank">
+                <a href="/collection/print" class="p-4 hover:text-highlight mr-1" target="_blank" title="Print collection">
                     <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 32 32">
                         <path d="M8 2h16v4h-16v-4z"></path>
                         <path d="M30 8h-28c-1.1 0-2 0.9-2 2v10c0 1.1 0.9 2 2 2h6v8h16v-8h6c1.1 0 2-0.9 2-2v-10c0-1.1-0.9-2-2-2zM4 14c-1.105 0-2-0.895-2-2s0.895-2 2-2 2 0.895 2 2-0.895 2-2 2zM22 28h-12v-10h12v10z"></path>
                     </svg>
                 </a>
-                <a href="/collection/missing" class="p-4 hover:text-highlight" target="_blank">
+                <a href="/collection/missing" class="p-4 hover:text-highlight" target="_blank" title="Stamps missing from collection">
                     <svg class="fill-current mr-2" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 32 32">
                         <path d="M16 2.899l13.409 26.726h-26.819l13.409-26.726zM16 0c-0.69 0-1.379 0.465-1.903 1.395l-13.659 27.222c-1.046 1.86-0.156 3.383 1.978 3.383h27.166c2.134 0 3.025-1.522 1.978-3.383h0l-13.659-27.222c-0.523-0.93-1.213-1.395-1.903-1.395v0z"></path>
                         <path d="M18 26c0 1.105-0.895 2-2 2s-2-0.895-2-2c0-1.105 0.895-2 2-2s2 0.895 2 2z"></path>
