bf69e5a780e9533ea27dc5d3adc08705f66859d8
Michael Perks <michael.perks@live.co.uk>
2021-01-27 20:00:35 +0000

    Extract Metadata for issues

--- app/Console/Commands/ExtractIssueMetaData.php
+++ app/Console/Commands/ExtractIssueMetaData.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Issue;
+use App\IssueCategory;
+use Illuminate\Support\Carbon;
 use Illuminate\Console\Command;
 
 class ExtractIssueMetaData extends Command
@@ -21,6 +23,8 @@ class ExtractIssueMetaData extends Command
      */
     protected $description = 'Extract issue metadata from description';
 
+    protected $categories;
+
     /**
      * Create a new command instance.
      *
@@ -29,6 +33,8 @@ class ExtractIssueMetaData extends Command
     public function __construct()
     {
         parent::__construct();
+
+        $this->categories = IssueCategory::all();
     }
 
     /**
@@ -64,17 +70,44 @@ class ExtractIssueMetaData extends Command
 
     private function extractMetaData(&$issue)
     {
-        $category = "/(Commemorative|Definitive|Post & Go)[\n|\r]?/i";
-        $designer = "/\bDesigned by\b (.+)[\n|\r]?/i";
-        $size = "/\bSize\b (.+)[\n|\r]?/i";
-        $printer = "/\bPrinted By\b (.+)[\n|\r]?/i";
-        $print_process = "/\bPrint Process\b (.+)[\n|\r]?/i";
-        $perforations = "/\bPerforations\b (.+)[\n|\r]?/i";
-        $gum = "/\bGum\b (.+)[\n|\r]?/i";
+        $category = "/(Commemorative|Definitive|Post & Go|Machin Definitive)/i";
+        $designer = "/Designed by (.*?)(?:<br\s*\/>)(?:\\r)/i";
+        $size = "/Size (.*?)(?:<br\s*\/>)(?:\\r)/i";
+        $printer = "/Printed by (.*?)(?:<br\s*\/>)(?:\\r)/i";
+        $print_process = "/Print Process (.*?)(?:<br\s*\/>)(?:\\r)/i";
+        $perforations = "/Perforations (.*?)(?:<br\s*\/>)(?:\\r)/i";
+        $gum = "/Gum (.*?)(?:<br\s*\/>)(?:\\r)/i";
 
-        dump($issue->getAttributes());
+        $issue->description = nl2br($issue->description);
+        // Add neew line to end of string to be able to capture last item
+        $issue->description .= "<br />\r";
 
-        // TODO: Category
+        // Release Date
+        $release_date = Carbon::create($issue->release_date);
+
+        if ($release_date->greaterThan(Carbon::create('6 February 1952'))) {
+            $issue->monarch_id = 6;
+        } elseif ($release_date->betweenIncluded(Carbon::create('11 December 1936'), Carbon::create('5 February 1952'))) {
+            $issue->monarch_id = 5;
+        } elseif ($release_date->betweenIncluded(Carbon::create('20 January 1936'), Carbon::create('10 December 1936'))) {
+            $issue->monarch_id = 4;
+        } elseif ($release_date->betweenIncluded(Carbon::create('6 May 1910'), Carbon::create('19 January 1936'))) {
+            $issue->monarch_id = 3;
+        } elseif ($release_date->betweenIncluded(Carbon::create('22 January 1901'), Carbon::create('5 May 1910'))) {
+            $issue->monarch_id = 2;
+        } elseif ($release_date->betweenIncluded(Carbon::create('20 June 1837'), Carbon::create('21 January 1901'))) {
+            $issue->monarch_id = 1;
+        }
+
+        preg_match($category, $issue->description, $categoryMatches);
+        if ($categoryMatches) {
+            $findCategory = trim($categoryMatches[1]);
+            $foundCategory = $this->categories->first(function ($category) use ($findCategory) {
+                return strtolower($findCategory) === strtolower($category->category);
+            });
+            $issue->category_id = $foundCategory->id ?? null;
+            $issue->description = str_replace($categoryMatches[0], '', $issue->description);
+        }
 
         preg_match($designer, $issue->description, $designerMatches);
         if ($designerMatches) {
@@ -102,7 +135,7 @@ class ExtractIssueMetaData extends Command
 
         preg_match($perforations, $issue->description, $perforationsMatches);
         if ($perforationsMatches) {
-            $issue->perforation = trim($perforationsMatches[1]);
+            $issue->perforations = trim($perforationsMatches[1]);
             $issue->description = str_replace($perforationsMatches[0], '', $issue->description);
         }
 
@@ -112,9 +145,10 @@ class ExtractIssueMetaData extends Command
             $issue->description = str_replace($gumMatches[0], '', $issue->description);
         }
 
-        // Final trim
+        // Final trims
+        $issue->description = preg_replace("/(<br\s*\/>)+/", '', $issue->description);
         $issue->description = trim($issue->description);
 
-        dump($issue->getAttributes());
+        $issue->save();
     }
 }
